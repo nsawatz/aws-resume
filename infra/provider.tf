db3a8593875566ac8c70a5157c0016e499c81376
nsawatz <nsawatz@wgu.edu>
2025-01-10 18:57:26 -0700

    grad_update_2

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,14 +1,12 @@
 terraform {
     required_providers {
         aws = {
-            version ="≥4.9.0"
+            version ="5.82.2"
             source = "hashicorp/aws"
         }
     }
 }
 provider "aws" {
-    profile = "your_cli_profile"
-    access_key = "your_access_key"
-    secret_key = "your_secret_key"
-    Region = "ca-central-1"
+    profile = "default"
+    region = "us-east-2"
 }
